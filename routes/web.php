79fc868ba193b8726bfd10294078be2675f8b836
Ferelyn Mae T. Gabitano <fgabitano@ssct.edu.ph>
2025-06-10 17:13:40 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,12 @@ use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\LearnMoreController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/welcome', [WelcomeController::class, 'index'])->name('welcome');
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
-Route::get('/learnmore', [LearnMoreController::class, 'index'])->name('learnmore');
-Route::get('/admin_dashboard', [WelcomeController::class, 'index'])->name('admin_dashboard'); // Placeholder
-Route::get('/mem_dashboard', [WelcomeController::class, 'index'])->name('mem_dashboard'); // Placeholder
-Route::get('/faci_dashboard', [WelcomeController::class, 'index'])->name('faci_dashboard'); // Placeholder
+Route::get('/welcome', [WelcomeController::class, 'welcome'])->name('welcome');
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::get('/learnmore', [LearnMoreController::class, 'learnmore'])->name('learnmore');
+Route::get('/admin_dashboard', [WelcomeController::class, 'admin_dashboard'])->name('admin_dashboard'); // Placeholder
+Route::get('/mem_dashboard', [WelcomeController::class, 'mem_dashboard'])->name('mem_dashboard'); // Placeholder
+Route::get('/faci_dashboard', [WelcomeController::class, 'faci_dashboard'])->name('faci_dashboard'); // Placeholder
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 Route::get('/project/{id}', [ProjectController::class, 'show'])->name('project.details')->where('id', '[1-5]');
